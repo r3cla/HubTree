a7358dc7f849aef28c25b57f3159f7b9686ec815
Nathan Logie <itmademe49@gmail.com>
2025-07-14 17:18:27 +1200

    minor edits

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "no-unused-vars": "off",
-      "@typescript-eslint/no-explicit-any": "off"  // Add this line
+      "@typescript-eslint/no-explicit-any": "off" 
     }
   }
 ];
