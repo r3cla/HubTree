9f550058a1feaf8df79d538b8a59875cc861ae97
Nathan Logie <itmademe49@gmail.com>
2026-04-21 08:14:14 +1200

    wire up Geist fonts in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "sans-serif"],
+        mono: ["var(--font-geist-mono)", "monospace"],
+      },
     },
   },
   plugins: [],
